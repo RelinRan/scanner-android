fc0ccc5ff28ea9e1cf0ba1047052ee943a0a38e7
Relin <1101005304@qq.com>
2026-08-01 14:50:12 +0800

    fix: restore BarcodeFormat in public package

---
+++ app/src/main/java/android/scanner/api/BarcodeFormat.kt
@@ -0,0 +1,17 @@
+package android.scanner.api
+
+public enum class BarcodeFormat {
+    Aztec,
+    Codabar,
+    Code39,
+    Code93,
+    Code128,
+    DataMatrix,
+    Ean8,
+    Ean13,
+    Itf,
+    Pdf417,
+    QrCode,
+    UpcA,
+    UpcE,
+}
